db6424edda1d8eadd5584e3d84489b7b0ca20e7b
MaksimB96 <>
2023-07-26 17:00:41 +0000

    Add: products html/url

--- products/views.py
+++ products/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+form .models import Product
 
-# Create your views here.
+
+def all_products(request):
+    """View that renders all Products, and handles sorting/searching"""
+
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/products.html', context)
