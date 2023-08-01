6eea8bbb284b1eeedff57c6d24b0da80a4a0e5e2
MaksimB96 <>
2023-08-01 19:05:10 +0000

    Add: bag templates folder with bag.html, add basic view render to bag/views.py

--- bag/views.py
+++ bag/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def view_bag(request):
+    """A view that dis[lays the contents of shopping bag"""
+
+    return render(request, 'bag/bag.html')
