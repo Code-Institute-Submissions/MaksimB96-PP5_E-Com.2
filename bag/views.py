da76e24c21862c02418abc6da382a96de521ea34
MaksimB96 <>
2023-08-03 17:04:10 +0000

    Add: product size categories to product detail, adjust model to reflect sizes

--- bag/views.py
+++ bag/views.py
@@ -8,12 +8,17 @@ def view_bag(request):
 
 
 def add_to_bag(request, item_id):
-    """Allows for adding items to shooping bag"""
+    """Allows for adding items to shopping bag"""
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['size']
     bag = request.session.get('bag', {})
 
+    if size:
+        
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
     else:
