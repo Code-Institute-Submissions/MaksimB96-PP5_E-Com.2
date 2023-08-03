dc9ed6237c46b2965245b45ba52bf948bfafeee8
MaksimB96 <>
2023-08-03 16:26:41 +0000

    Add: sizes to products model

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 
 
 def view_bag(request):
@@ -22,3 +22,26 @@ def add_to_bag(request, item_id):
     request.session['bag'] = bag
     return redirect(redirect_url)
 
+
+def adjust_bag(request, item_id):
+    """Allows for adjusting quantities in the bag"""
+
+    quantity = int(request.POST.get('quantity'))
+    bag = request.session.get('bag', {})
+
+    if quantity > 0:
+        bag[item_id] = quantity
+    else:
+        bag.pop[item_id]
+
+    request.session['bag'] = bag
+    return redirect(reverse("view_bag"))
+
+
+def remove_from_bag(request, item_id):
+    """Allows for removing items from shopping bag"""
+
+    
+
+    request.session['bag'] = bag
+    return HttpResponse(status=200)
