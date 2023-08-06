6125f21a9502b6c659d879761c40fc28adaa65d9
MaksimB96 <maksims.buraks.1996@gmail.com>
2023-08-06 13:46:57 +0000

    Fix: checkout success screen and fixed css not loading correctly

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -48,7 +48,8 @@ form.addEventListener('submit', function (ev) {
     ev.preventDefault();
     card.update({'disabled': true});
     $('#submit-button').attr('disabled', true)
-
+    $('#payment-form').fadeToggle(100);
+    $('#loading-screen').fadeToggle(100);
     stripe.confirmCardPayment(clientSecret, {
         payment_method: {
             card: card,
@@ -61,8 +62,9 @@ form.addEventListener('submit', function (ev) {
                 <i class="fas fa-times"></i>
             </span>
             <span>${result.error.message}</span>`;
-    
             $(errorDiv).html(html);
+            $('#payment-form').fadeToggle(100);
+            $('#loading-screen').fadeToggle(100);
             card.update({'disabled': false});
             $('#submit-button').attr('disabled', false)
         } else {
