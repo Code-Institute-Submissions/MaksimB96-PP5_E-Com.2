1f6882bdc697fc5c28c886bef90f4c4fa0dd2e32
MaksimB96 <maksims.buraks.1996@gmail.com>
2023-08-08 12:42:45 +0000

    Add: profile form render and Fix: nave typo

--- products/forms.py
+++ products/forms.py
@@ -1,12 +1,11 @@
 from django import forms
-
-form .models import Product, Category
-
+from .models import Product, Category
 
 class ProductForm(forms.ModelForm):
 
     class Meta:
-        model = ProductFormfields = '__all__'
+        model = Product
+        fields = '__all__'
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
