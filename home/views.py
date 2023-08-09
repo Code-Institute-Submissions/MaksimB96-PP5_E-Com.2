94bd96c12bc59f4880809e9d47ad6bccf1d71b64
MaksimB96 <maksims.buraks.1996@gmail.com>
2023-08-09 20:03:48 +0000

    Add considerable adjustments to index and implemented products to render

--- home/views.py
+++ home/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from products.models import Product
 
 
 def index(request):
     """Main index page render"""
+ 
+    products = Product.objects.filter(category__name='honey')
+    context = {
+        'products': products
+    }
     
-    return render(request, 'home/index.html')
+    return render(request, 'home/index.html', context)
