542b4d0bd5dc93737a1a2148b85a6d02f20c294d
MaksimB96 <maksims.buraks.1996@gmail.com>
2023-08-10 17:28:01 +0000

    Add: cache control

--- honey_haven/settings.py
+++ honey_haven/settings.py
@@ -203,6 +203,12 @@ STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 # AWS set up
 
 if 'USE_AWS' in os.environ:
+
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
     AWS_STORAGE_BUCKET_NAME = 'maksimb96-honeyhaven'
     AWS_S3_REGION_NAME = 'eu-west-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
