101a29bf6df832f42249ae8ee8d678bc2508d91a
MaksimB96 <maksims.buraks.1996@gmail.com>
2023-08-10 12:25:54 +0000

    Add: changes to environ vars

--- honey_haven/settings.py
+++ honey_haven/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3f%cr%fb@#e(+0j#9=08)#1be%tzb5egkd!w$@zpiqu+78ivws'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['8000-maksimb96-pp5ecom-pe31lc9k99j.ws-eu102.gitpod.io', 'honeyhaven-236ab8f8ceba.herokuapp.com']
 
