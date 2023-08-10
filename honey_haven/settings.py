9277232c5454ba385c6fa0c39224fc561466c29c
MaksimB96 <maksims.buraks.1996@gmail.com>
2023-08-10 18:50:22 +0000

    Add: email hosting variables

--- honey_haven/settings.py
+++ honey_haven/settings.py
@@ -225,4 +225,14 @@ if 'USE_AWS' in os.environ:
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
 #Emailing
-DEFAULT_FROM_EMAIL = 'honeyhaven@example.com'
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'honeyhaven@example.com'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    EMAIL_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
