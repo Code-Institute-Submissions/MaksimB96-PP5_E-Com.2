a88bbb750d50797881d2b4867312c979b59d9204
MaksimB96 <maksims.buraks.1996@gmail.com>
2023-08-11 20:36:19 +0000

    Add: admin handler for news

--- subscription/views.py
+++ subscription/views.py
@@ -1,9 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.core.mail import send_mail
+
+from .forms import SubscriberAdminNews
+from .models import Subscribers
 
 
 def admin_news_form(request):
     """Handles The admin news section and sends mail"""
-    
+    form = SubscriberAdminNews()
+    if request.method == 'POST':
+        form = SubscriberAdminNews(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'News Article has been sent to all subscribers!')
+            return redirect('admin_news')
+        else:
+            form = SubscriberAdminNews()
 
-    return render(request, 'subscription/newsletter.html')
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'subscription/newsletter.html', context)
 
