1a66192e9e33755306a640173fb44ab20d0e5300
MaksimB96 <maksims.buraks.1996@gmail.com>
2023-08-11 21:09:00 +0000

    Add: finish implemeting News letter

--- subscription/views.py
+++ subscription/views.py
@@ -2,24 +2,42 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.core.mail import send_mail
 
+from django_pandas.io import read_frame
+
 from .forms import SubscriberAdminNews
 from .models import Subscribers
 
 
 def admin_news_form(request):
     """Handles The admin news section and sends mail"""
-    form = SubscriberAdminNews()
+
+    email_subs = Subscribers.objects.all()
+    data_frame = read_frame(email_subs, fieldnames=['email'])
+    mail_list = data_frame['email'].values.tolist()
+    forms = SubscriberAdminNews()
+
     if request.method == 'POST':
-        form = SubscriberAdminNews(request.POST)
-        if form.is_valid():
-            form.save()
+        forms = SubscriberAdminNews(request.POST)
+        if forms.is_valid():
+            forms.save()
+            title = forms.cleaned_data.get('title')
+            news_body = forms.cleaned_data.get('news_body')
+
+            send_mail(
+                title,
+                news_body,
+                settings.EMAIL_HOST_USER,
+                mail_list,
+                fail_silently=False,
+            )
+
             messages.success(request, 'News Article has been sent to all subscribers!')
             return redirect('admin_news')
         else:
-            form = SubscriberAdminNews()
+            forms = SubscriberAdminNews()
 
     context = {
-        'form': form,
+        'forms': forms,
     }
 
     return render(request, 'subscription/newsletter.html', context)
