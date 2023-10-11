e88bd1b3803f4adbb8595839f6456b6ca055f9d2
MaksimB96 <maksims.buraks.1996@gmail.com>
2023-10-11 10:00:14 +0000

    Fix: issue with confirmation email, due to typo

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -30,7 +30,7 @@ class StripeWH_Handler:
             'checkout/confirmation_emails/confirmation_email_body.txt',
             {'order': order, 'contact_email': settings.DEFAULT_FROM_EMAIL}
         )
-        send_mail = (
+        send_mail(
             subject,
             body,
             settings.DEFAULT_FROM_EMAIL,
